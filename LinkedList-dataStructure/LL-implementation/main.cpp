5c5b3addbd297e37f90333c17ae270b4e8299a4c
sunwoongc <78211057+sunwoongc@users.noreply.github.com>
2021-02-16 15:44:50 +0900

    Update main.cpp
    In void removeNode(int pos), if pos is bigger than 1, and less than the total nodes (the usual case), it should start with i = 1, since you consider the first index as pos 1.
    
    For example, assume that there is a list 1-->2-->3-->4-->5-->6-->.
    Then if we want to remove 3, the 3rd position we should operate l.removeNode(2) in the original code.
    For the cases above, I changed the code started from i = 1.

--- LinkedList-dataStructure/LL-implementation/main.cpp
+++ LinkedList-dataStructure/LL-implementation/main.cpp
@@ -124,7 +124,7 @@ public:
             totalNodes--;
             return;
         }
-        for (int i = 0; temp != NULL && i < pos - 1; i++)
+        for (int i = 1; temp != NULL && i < pos - 1; i++)
         {
             temp = temp->link;
         }
