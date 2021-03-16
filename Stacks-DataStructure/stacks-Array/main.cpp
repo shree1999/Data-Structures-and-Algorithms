46df9a22f0c6ff41aaf9c0338e8624673d8a88cc
Vikas Tiwari <51874681+vikkastiwari@users.noreply.github.com>
2021-03-16 16:06:00 +0530

    Update main.cpp

--- Stacks-DataStructure/stacks-Array/main.cpp
+++ Stacks-DataStructure/stacks-Array/main.cpp
@@ -1,70 +1,93 @@
-/* Stack Implementation using Array
-In this implementation we are initializing top as our moving index
-which will keep track of
-*/
+// stack using array
 
-#include <iostream>
-#include <cstdlib>
+#include <bits/stdc++.h>
 #define SIZE 128
 using namespace std;
 
-class Stack {
+class Stack
+{
+    // declaring values
     int top;
     int *arr;
+
 public:
-    Stack() {
+    // initailizing values
+    Stack()
+    {
         top = -1;
         arr = new int[SIZE];
     }
 
-    void push(int data) {
-        // this function allows to put data into top of the stack
-        if(top >= SIZE - 1) { // checking for over-flow
-            cout<<"Stack Over-Flows"<<endl;
+    // function to push data into stack-[array]
+    void push(int data)
+    {
+        // Overflow check in case the limit exceeds
+        if (top >= SIZE - 1)
+        {
+            cout << "[WRANING]Stack Overflows" << endl;
             return;
         }
-        top = top + 1;
+        // increment the counter and add the data
+        top += 1;
         arr[top] = data;
     }
 
-    int pop() {
-        if(top == -1) {
-            cout<<"Stack Under-Flow"<<endl;
-            return -1;
+    // function pops the top most element
+    void pop()
+    {
+        // checking whether the stack is empty or not
+        if (top == -1)
+        {
+            cout << "[WARNING]Stack is empty." << endl;
+            return;
         }
-        int data = arr[top];
+        // if its not empty display the top element and decrement top by 1 to delete the top most data
+        cout << "[INFO]Popped Element: " << arr[top] << endl;
         top--;
-        return data;
     }
 
-    int peek() {
+    // function used to peek onto the top most element
+    int peek()
+    {
         return arr[top];
     }
 
-    void display() {
-        if(top >= 0) {
-                cout<<"The Stack elements are: "<<endl;
-            for(int i = top; i >= 0; i--) {
-                cout<<arr[i]<<"  ";
+    void display()
+    {
+        // looping over the stack conditionally
+        if (top >= 0)
+        {
+            cout << "The Stack elements are: " << endl;
+            for (int i = top; i >= 0; i--)
+            {
+                cout << arr[i] << "  ";
             }
-            cout<<endl;
+            cout << endl;
         }
-        else {
-            cout<<"Please Fill the elements first"<<endl;
+        // if stack is empty then else block is executed
+        else
+        {
+            cout << "[WARNING]Please fill the elements first" << endl;
             return;
         }
     }
 };
 
-int main() {
+int main()
+{
     Stack s;
-    s.push(20);
-    s.push(30);
-    s.push(40);
-    s.push(50);
-    s.push(60);// note the output 60 will come first even if it was added at last.
-
-    cout<<s.pop()<<endl;
+    s.push(12);
+    s.push(11);
+    s.push(13);
+    s.push(33);
+    s.push(02);
     s.display();
-    return 0;
+
+    s.pop();
+    s.display();
+
+    s.push(24);
+    s.display();
+
+    cout << "Top most element: " << s.peek() << endl;
 }
