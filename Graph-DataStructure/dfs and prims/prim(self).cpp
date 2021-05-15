cc318cda3de3942726f3596cbc97e35293dbf2a5
Harsh Sachin Chandekar <59416466+harshchan@users.noreply.github.com>
2021-05-15 17:36:21 +0530

    Update prim(self).cpp

--- Graph-DataStructure/dfs and prims/prim(self).cpp
+++ Graph-DataStructure/dfs and prims/prim(self).cpp
@@ -3,10 +3,10 @@
 #include<stdio.h>
 #include<iostream>
 
-#define V 6
+#define V 6	//6 vertices/node in graph
 using namespace std;
 
-int minKey(int key[],bool mstSet[])
+int minKey(int key[],bool mstSet[])	//return minimum element among all elements in mstSet
 {
 	int min=1000,min_index;
 	for(int v=0;v<V;v++)
@@ -18,7 +18,7 @@ int minKey(int key[],bool mstSet[])
 	return min_index;
 }
 
-int printMST(int parent[],int graph[V][V])
+int printMST(int parent[],int graph[V][V])//print minimum spanning tree
 {
 	cout<<"Edge \tWeight\n";
 	
@@ -29,20 +29,20 @@ int printMST(int parent[],int graph[V][V])
 }
 
 
-void prim(int graph[V][V])
+void prim(int graph[V][V])	
 {
 	int parent[V];
 	int key[V];
 	bool mstSet[V];
 	
-	for(int i=0;i<V;i++)
+	for(int i=0;i<V;i++)		
 	{
 		key[i]=1000,mstSet[i]=false;
 		key[0]=0;
 		parent[0]=-1;
 		for(int count=0;count<V-1;count++)
 		{
-			int u=minKey(key,mstSet);
+			int u=minKey(key,mstSet);//find minimum edge cost and add in mst
 			mstSet[u]=true;
 			
 			for(int v=0;v<V;v++)
